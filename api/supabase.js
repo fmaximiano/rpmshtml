5fcad11700853cc4d314e40a63a88f34455af6f3
Frederico Maximiano <f.a.maximiano@gmail.com>
2025-03-04 20:46:17 -0300

    add supabase.js

---
+++ api/supabase.js
@@ -0,0 +1,16 @@
+import { createClient } from "@supabase/supabase-js";
+
+export default async function handler(req, res) {
+    const supabase = createClient(
+        process.env.SUPABASE_URL, 
+        process.env.SUPABASE_KEY
+    );
+
+    const { data, error } = await supabase.from("licencas").select("*");
+
+    if (error) {
+        return res.status(500).json({ error: error.message });
+    }
+
+    res.status(200).json(data);
+}
